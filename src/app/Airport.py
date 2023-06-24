c1176e2101526dc78fc61b8ea69da4f4b11bb918
Melissa <123083767+melissaSanchez05@users.noreply.github.com>
2023-06-24 18:01:42 +0000

    airport ~

---
+++ src/app/Airport.py
@@ -0,0 +1,9 @@
+class Airport:
+    def __init__(self,code, initLatitudeDegrees, initLatitudeMinutes, initLongitudeDegrees, initLongitudeMinutes):
+        self.code = code
+        self.initLatitudeDegrees = initLatitudeDegrees
+        self.initLatitudeMinutes = initLatitudeMinutes
+        self.initLongitudeDegrees = initLongitudeDegrees
+        self.initLongitudeMinutes = initLongitudeMinutes
+
+
