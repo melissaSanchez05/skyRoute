72db529ffd6d1e15ade6af30042c0e21610f56a8
Melissa <123083767+melissaSanchez05@users.noreply.github.com>
2023-06-25 17:22:23 +0000

    displayMenu done

--- src/app/FligthPlanner.py
+++ src/app/FligthPlanner.py
@@ -27,18 +27,22 @@ class FlightPlanner():
         self.inputStack = 0
         self.graph = {}
         self.totalTripMilage = []
-        totalMilage = 0
         tps = cppTPS()
 
     def displayMenu():
-        pass
+        menu = "ENTER A SELECTION \n"
+        menu+= "S) Add a Stop to your Trip \n"
+        menu+="U) Undo \n"
+        menu+="E) Empty Trip \n"
+        menu+="Q) Quit \n"
+        mernu+="-"
+        return menu
     def initAllAirports():
         pass
     def displayAirports():
         pass
     def processUserInput(self):
         entry = input()
-
         if entry == "S":
             
             airportId = input("\nEnter the airport ID: \n")
@@ -63,16 +67,62 @@ class FlightPlanner():
             self.tps.addTransaction(transaction)
 
         elif entry == "U":
-            self.tps.undoTransaction()
-            self.inputStack-=1
+            if (len(self.inputStops) - self.inputStack) > 0:
+                self.tps.undoTransaction()
+                self.inputStack-=1
+            else:
+                print("There are not transaction to undo.\n")
         elif entry == "R":
             if self.tps.hasTransactionRedo():
                 self.tps.doTransaction()
                 self.inputStack-=1
+            else:
+                 print("There are not transaction to redo.\n")
         elif entry == "E":
+            self.tps.clearAllTransactions()
+            self.inputStops.clear()
+            self.inputStack = 0
+        elif entry == "Q":
+            return False
+        else:
+            print("Selection incorrect.\n")
+
+        self.totalTripMilage.clear()
+        return True
                 
-    def displayCurrentTrip():
-        pass
+    def displayCurrentTrip(self):
+        print("Trip Stops: \n")
+        if (len(self.inputStops) - self.inputStack) > 0:
+
+            for i in range(self.inputStack, len(self.inputStops)):
+                print(str(i + 1 ) +". " + self.inputStack[i])
+
+        print("\n")
+        print("Trip legs: \n")
+        if (len(self.inputStops) - self.inputStack) > 1:
+            counter = 1
+            dash = 0
+
+            for i in range(self.inputStack, len(self.inputStops) - 1):
+                route = self.graph.findPath(self.inputStops[i], self.inputStops[i + 1])
+                print(str(counter) + ". ")
+
+                for stop in route:
+                    print(stop)
+                    if (len(route) - 1) > dash:
+                        print("-")
+                        dash+=1
+                print(" ( " + str(self.graph.getDistance()) + " Miles\n")
+                self.totalTripMilage.append(self.graph.getDistance())
+                counter+=1
+                dash = 0
+                    
+        print("Total distance: \n")
+        tripDistance = 0
+        for miles in self.totalTripMilage:
+            tripDistance+=miles
+        
+        print(str(tripDistance) + " Miles\n")
 
     def main():
         pass
